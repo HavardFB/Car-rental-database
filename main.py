b1dcc655fd9b2fd18fda20b3abb0b9743f5762e2
Håvard Fjellbakken <91278949+HavardFB@users.noreply.github.com>
2022-05-03 20:19:40 +0200

    Update main.py
    Comments

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import sys
-
 from view.menu_screens import main_menu
 from controllers.database_controller import DatabaseController
 
 
 if __name__ == "__main__":
+    # Creates an instance of the DatabaseController
     db_controller = DatabaseController(r"database\car-rental.db")
+
+    # Calls the main menu screen function
     main_menu(db_controller)
+
+    # Exiting the program
     db_controller.close_connection()
     sys.exit()
