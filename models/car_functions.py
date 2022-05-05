86beefb3846ad151b4a489f7c33e8f514e627602
Håvard Fjellbakken <91278949+HavardFB@users.noreply.github.com>
2022-05-05 16:47:33 +0200

    Formatting

--- models/car_functions.py
+++ models/car_functions.py
@@ -13,15 +13,17 @@ def add_car(db_controller):
 
     # Inserting data this way to ensure fields are inserted as data and not commands
     db_controller.execute_query(
-        f"INSERT INTO car (make, model, plate, year, color, mileage)VALUES (?, ?, ?, ?, ?, ?)",
-        (make, model, plate, year, color, mileage)
+        f"INSERT INTO car (make, model, plate, year, color, mileage) VALUES (?, ?, ?, ?, ?, ?)",
+        (make, model, plate, year, color, mileage),
     )
 
 
 def edit_car(db_controller):
     car_id = integer_input("Enter the ID of the car you want to edit: ")
     # First check if it exists
-    car = db_controller.execute_single_read_query(f"SELECT make, model, plate FROM car WHERE id = ?", (car_id,))
+    car = db_controller.execute_single_read_query(
+        f"SELECT make, model, plate FROM car WHERE id = ?", (car_id,)
+    )
     if car is None:
         print("There is no car with that ID.")
     else:
@@ -35,16 +37,20 @@ def edit_car(db_controller):
         db_controller.execute_query(
             f"UPDATE car SET make = ?, model = ?, plate = ?, "
             f"year = ?, color = ?, mileage = ? WHERE id = ?",
-            (make, model, plate, year, color, mileage, car_id)
+            (make, model, plate, year, color, mileage, car_id),
         )
 
 
 def remove_car(db_controller):
     car_id = integer_input("Enter the ID of the car you want to remove: ")
     # First check if it exists
-    car = db_controller.execute_single_read_query(f"SELECT make, model, plate, year FROM car WHERE id = ?", (car_id,))
+    car = db_controller.execute_single_read_query(
+        f"SELECT make, model, plate, year FROM car WHERE id = ?", (car_id,)
+    )
     if car is None:
         print("There is no car with that ID.")
     else:
         db_controller.execute_query(f"DELETE FROM car WHERE id = ?", (car_id,))
-        print(f"Deleted {car[3]} model {car[0]} {car[1]}, {car[2]}.")    # year, make, model, plate
+        print(
+            f"Deleted {car[3]} model {car[0]} {car[1]}, {car[2]}."
+        )  # year, make, model, plate
