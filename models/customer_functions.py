fdd87ce702837032e612510563698af56edea42a
Håvard Fjellbakken <91278949+HavardFB@users.noreply.github.com>
2022-05-22 14:35:28 +0200

    checks if customer has a rental before removing, and deletes rental history when removing customer

--- models/customer_functions.py
+++ models/customer_functions.py
@@ -1,4 +1,3 @@
-from controllers.user_input import name_input
 from controllers.user_input import string_input
 from controllers.user_input import phone_input
 from controllers.user_input import year_input
@@ -6,10 +5,10 @@ from controllers.user_input import integer_input
 
 
 def add_customer(db_controller):
-    first_name = name_input("Enter first name (leave blank to cancel): ")
+    first_name = string_input("Enter first name (leave blank to cancel): ")
     if first_name is None:
         return
-    last_name = name_input("Enter last name (leave blank to cancel): ")
+    last_name = string_input("Enter last name (leave blank to cancel): ")
     if last_name is None:
         return
     email = string_input(
@@ -46,10 +45,10 @@ def edit_customer(db_controller):
         if customer is None:
             print("There is no customer with that ID")
         else:
-            first_name = name_input("Enter new first name (leave blank to cancel): ")
+            first_name = string_input("Enter new first name (leave blank to cancel): ")
             if first_name is None:
                 return
-            last_name = name_input("Enter new last name (leave blank to cancel): ")
+            last_name = string_input("Enter new last name (leave blank to cancel): ")
             if last_name is None:
                 return
             email = string_input("Enter new email (leave blank to cancel): ")
@@ -62,11 +61,16 @@ def edit_customer(db_controller):
             if birth_year is None:
                 return
 
-            # Updates database
+            # Updates the customer table
             db_controller.execute_query(
                 f"UPDATE customer SET first_name = ?, last_name = ?, email = ?, phone_number = ?, "
                 f"birth_year = ? WHERE customer_id = ?",
                 (first_name, last_name, email, phone, birth_year, customer_id),
+            )
+            # Updates the rental table with the new last name and phone number
+            db_controller.execute_query(
+                f"UPDATE rental SET customer_last_name = ?, customer_phone_number = ? WHERE customer_id = ?",
+                (last_name, phone, customer_id),
             )
             return
 
@@ -78,15 +82,30 @@ def remove_customer(db_controller):
         if customer_id is None:
             return
         else:
-            # First check if it exists
+            # First checks if he/she exists
             customer = db_controller.execute_single_read_query(
                 f"SELECT first_name, last_name FROM customer WHERE customer_id = ?", (customer_id,)
             )
             if customer is None:
                 print("There is no customer with that ID")
+                return
+            # Checks if there are no rentals for this customer
+            elif db_controller.execute_single_read_query(
+                f"SELECT customer_id FROM rental WHERE customer_id = ?", (customer_id,)
+            ):
+                print("This customer has rentals that need to be returned first!")
+                return
             else:
+                # Deletes customer from the customer table
                 db_controller.execute_query(
                     f"DELETE FROM customer WHERE customer_id = ?", (customer_id,)
+                )
+                # The next command deletes the rental history for this customer, the idea is that when cars are
+                # returned the customers will not be removed by default. This is to keep the rental history in the
+                # database. If the customer asks to be removed then the rental history also needs to be removed to
+                # delete everything identifying this customer to comply with the law.
+                db_controller.execute_query(
+                    f"DELETE FROM rental WHERE customer_id = ?", (customer_id,)
                 )
                 print(
                     f"Customer {customer[0]} {customer[1]} has been removed"
@@ -99,7 +118,7 @@ def list_customers(db_controller):
         "SELECT customer_id, first_name, last_name FROM customer", ()
     )
     if customers:
-        print("Customers:")
+        print("Current customers:")
         print("ID | Name")
         for customer in customers:
             print(f"{customer[0]}: {customer[1]} {customer[2]}")
