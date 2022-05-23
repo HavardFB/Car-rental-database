1dd64ed923cc92e4763a56277953f80408c8c8a6
Håvard Fjellbakken <91278949+HavardFB@users.noreply.github.com>
2022-05-24 00:20:45 +0200

    Changed to seperate import/export menus + formatting

--- view/menu_screens.py
+++ view/menu_screens.py
@@ -15,13 +15,17 @@ def main_menu(db_controller):
         print(
             dedent(
                 """
+        MAIN MENU
+        ---------------------------------------------------
         Please select an option by entering the corresponding number:
         1. Customers
         2. Cars
         3. Rentals
         4. Search
-        5. Import/export data
-        6. Exit
+        5. Import data
+        6. Export data
+        7. Exit
+        ---------------------------------------------------
         """
             )
         )
@@ -30,8 +34,8 @@ def main_menu(db_controller):
         user_choice = integer_input("Enter your choice: ")
 
         # Makes sure the user enters a valid number
-        if user_choice in range(1, 7):
-            if user_choice == 6:
+        if user_choice in range(1, 8):
+            if user_choice == 7:
                 exit_application = True
                 print("Thank you for using the Car-rental system!")
             elif user_choice == 1:
@@ -43,7 +47,9 @@ def main_menu(db_controller):
             elif user_choice == 4:
                 search_menu(db_controller)
             elif user_choice == 5:
-                import_export_menu()
+                pass
+            elif user_choice == 6:
+                export_menu(db_controller)
         else:
             print("Your number is not in the menu range.")
 
@@ -56,11 +62,14 @@ def customer_menu(db_controller):
         print(
             dedent(
                 """
+        CUSTOMER MENU
+        ---------------------------------------------------
         What do you want to do with customers?
         1. Add new customer
         2. Edit existing customer
         3. Remove customer
         4. Return to main menu
+        ---------------------------------------------------
         """
             )
         )
@@ -90,11 +99,14 @@ def car_menu(db_controller):
         print(
             dedent(
                 """
+        CAR MENU
+        ---------------------------------------------------
         What do you want to do with cars?
         1. Add new car
         2. Edit existing car
         3. Remove car
         4. Return to main menu
+        ---------------------------------------------------
         """
             )
         )
@@ -124,10 +136,13 @@ def rental_menu(db_controller):
         print(
             dedent(
                 """
+        RENTAL MENU
+        ---------------------------------------------------
         What do you want to do with rentals?
         1. Assign a car to a customer
         2. Return a rented car
         3. Return to main menu
+        ---------------------------------------------------
         """
             )
         )
@@ -147,20 +162,20 @@ def rental_menu(db_controller):
             print("Your number is not in the menu range.")
 
 
-# Submenu for importing and exporting data
-def import_export_menu():
+def search_menu(db_controller):
     exit_menu = False
 
     while not exit_menu:
         print(
             dedent(
                 """
-        Please select an option:
-        1. Import data from CSV file
-        2. Export data to CSV file
-        3. Import data from JSON file
-        4. Export data to JSON file
-        5. Return to main menu
+        SEARCH MENU
+        ---------------------------------------------------
+        What do you want to search for?
+        1. Customers
+        2. Cars
+        3. Go back.
+        ---------------------------------------------------
         """
             )
         )
@@ -169,17 +184,17 @@ def import_export_menu():
         user_choice = integer_input("Enter your choice: ")
 
         # Makes sure the user enters a valid number
-        if user_choice in range(1, 6):
-            if user_choice == 5:
+        if user_choice in range(1, 4):
+            if user_choice == 3:
                 exit_menu = True
             elif user_choice == 1:
-                print("Importing from CSV file...")
+                print("Searching for customers...")
+                search_query = string_input("Please type your search query here: ")
+                search_customer(db_controller, search_query)
             elif user_choice == 2:
-                print("Exporting from CSV file...")
-            elif user_choice == 3:
-                print("Importing from JSON file...")
-            elif user_choice == 4:
-                print("Exporting from JSON file...")
+                print("Searching for cars...")
+                search_query = string_input("Please type your search query here: ")
+                search_car(db_controller, search_query)
         else:
             print("Your number is not in the menu range.")
 
