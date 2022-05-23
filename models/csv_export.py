4c6a94a6e8e7b8ab4eef4d48922338ec811991e8
Håvard Fjellbakken <91278949+HavardFB@users.noreply.github.com>
2022-05-24 00:54:49 +0200

    Added function to export cars

--- models/csv_export.py
+++ models/csv_export.py
@@ -20,3 +20,22 @@ def csv_export_customers(db_controller, file_name):
             for customer in customers:
                 file.write(str(customer[0]) + "," + customer[1] + "," + customer[2] + "," + customer[3] + "," + customer[4] + "," + str(customer[5]) + "\n")
 
+
+def csv_export_cars(db_controller, file_name):
+    if file_name is None:
+        file_name = "cars-" + str(get_date()) + ".csv"
+    else:
+        file_name += ".csv"
+
+    file_path = "exports/" + file_name
+
+    if exists(file_path):
+        print("ERROR: File with that name already exists!")
+        return -11
+    else:
+        # Opens the file and reads the columns into a customers list
+        with open(file_path, "w+") as file:
+            file.write("make,model,plate,year,color,mileage\n")
+            cars = db_controller.execute_read_query("SELECT make, model, plate, year, color, mileage FROM car", ())
+            for car in cars:
+                file.write(car[0] + "," + car[1] + "," + car[2] + "," + str(car[3]) + "," +car[4] + "," + str(car[5]) + "\n")
