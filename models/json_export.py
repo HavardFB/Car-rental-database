e9d36f9d2e27197d8561224ded4d74ff3583b8ae
Håvard Fjellbakken <91278949+HavardFB@users.noreply.github.com>
2022-05-25 15:05:26 +0200

    Moved user input inside the export function and moved list update outside of with open statement

--- models/json_export.py
+++ models/json_export.py
@@ -1,78 +1,121 @@
 import json
 from models.date import get_date
 from os.path import exists
+from controllers.user_input import string_input
 
 
-def json_export_customers(db_controller, file_name):
-    if file_name is None:
-        file_name = "customers-" + str(get_date()) + ".json"
-    else:
-        file_name = file_name + ".json"
+def json_export_customers(db_controller):
+    while True:
+        file_name = string_input("Please enter the filename without extensions (or press enter to use default name): ")
 
-    file_path = "exports/" + file_name
+        # Sets default file name
+        if file_name is None:
+            file_name = "customers-" + str(get_date()) + ".json"
+        # Adds .json extension
+        else:
+            file_name = file_name + ".json"
 
-    if exists(file_path):
-        print("ERROR: File with that name already exists!")
-        return -11
-    else:
-        with open(file_path, "w+", encoding="UTF8") as file:
+        file_path = "exports/" + file_name
+
+        if exists(file_path):
+            print("ERROR: File with that name already exists!")
+            continue
+        else:
             # List to contain the customers
             customers = []
 
             # Appends all customers to the list
-            for customer in db_controller.execute_read_query("SELECT first_name, last_name, email, phone_number, birth_year FROM customer", ()):
-                customers.append({"first_name": customer[0], "last_name": customer[1], "email": customer[2], "phone_number": customer[3], "birth_year": customer[4]})
+            for customer in db_controller.execute_read_query(
+                    "SELECT first_name, last_name, email, phone_number, birth_year FROM customer", ()):
+                customers.append({"first_name": customer[0], "last_name": customer[1], "email": customer[2],
+                                  "phone_number": customer[3], "birth_year": customer[4]})
 
-            # Writes the dictionary to the file
-            json_object = json.dumps(customers, indent=4, ensure_ascii=False)
-            file.write(json_object)
+            try:
+                # Opens file for writing
+                with open(file_path, "w+", encoding="UTF8") as file:
+                    # Writes the list to the file
+                    json_object = json.dumps(customers, indent=4, ensure_ascii=False)
+                    file.write(json_object)
+                print("Successfully exported to file " + file_name)
+                return
+            except Exception:
+                print(f"ERROR {Exception}: Something went wrong while writing to the file!")
+                return
 
 
-def json_export_cars(db_controller, file_name):
-    if file_name is None:
-        file_name = "cars-" + str(get_date()) + ".json"
-    else:
-        file_name = file_name + ".json"
+def json_export_cars(db_controller):
+    while True:
+        file_name = string_input("Please enter the filename without extensions (or press enter to use default name): ")
 
-    file_path = "exports/" + file_name
+        # Sets default file name
+        if file_name is None:
+            file_name = "cars-" + str(get_date()) + ".json"
+        # Adds .json extension
+        else:
+            file_name = file_name + ".json"
 
-    if exists(file_path):
-        print("ERROR: File with that name already exists!")
-        return -11
-    else:
-        with open(file_path, "w+", encoding="UTF8") as file:
+        file_path = "exports/" + file_name
+
+        if exists(file_path):
+            print("ERROR: File with that name already exists!")
+            continue
+        else:
             # List to contain the cars
             cars = []
 
             # Appends all cars to the list
             for car in db_controller.execute_read_query("SELECT make, model, plate, year, color, mileage FROM car", ()):
-                cars.append({"make": car[0], "model": car[1], "plate": car[2], "year": car[3], "color": car[4], "mileage": car[5]})
+                cars.append({"make": car[0], "model": car[1], "plate": car[2], "year": car[3], "color": car[4],
+                             "mileage": car[5]})
 
-            # Writes the dictionary to the file
-            json_object = json.dumps(cars, indent=4, ensure_ascii=False)
-            file.write(json_object)
+            try:
+                with open(file_path, "w+", encoding="UTF8") as file:
+                    # Writes the dictionary to the file
+                    json_object = json.dumps(cars, indent=4, ensure_ascii=False)
+                    file.write(json_object)
+                print("Successfully exported to file " + file_name)
+                return
+            except Exception:
+                print(f"ERROR {Exception}: Something went wrong while writing to the file!")
+                return
 
 
-def json_export_rental_history(db_controller, file_name):
-    if file_name is None:
-        file_name = "rental_history-" + str(get_date()) + ".json"
-    else:
-        file_name = file_name + ".json"
+def json_export_rental_history(db_controller):
+    while True:
+        file_name = string_input("Please enter the filename without extensions (or press enter to use default name): ")
 
-    file_path = "exports/" + file_name
+        # Sets default file name
+        if file_name is None:
+            file_name = "rental_history-" + str(get_date()) + ".json"
+        # Adds .json extension
+        else:
+            file_name = file_name + ".json"
 
-    if exists(file_path):
-        print("ERROR: File with that name already exists!")
-        return -11
-    else:
-        with open(file_path, "w+", encoding="UTF8") as file:
+        file_path = "exports/" + file_name
+
+        if exists(file_path):
+            print("ERROR: File with that name already exists!")
+            continue
+        else:
             # List to contain the rental history
             rental_history = []
 
             # Appends all rental history to the list
-            for rental in db_controller.execute_read_query("SELECT rental_date, return_date, customer_last_name, customer_phone_number, car_plate FROM rental WHERE return_date IS NOT NULL", ()):
-                rental_history.append({"rental_date": rental[0], "return_date": rental[1], "customer_last_name": rental[2], "customer_phone_number": rental[3], "car_plate": rental[4]})
+            for rental in db_controller.execute_read_query(
+                    "SELECT rental_date, return_date, customer_last_name, customer_phone_number, car_plate FROM rental "
+                    "WHERE return_date IS NOT NULL",
+                    ()):
+                rental_history.append(
+                    {"rental_date": rental[0], "return_date": rental[1], "customer_last_name": rental[2],
+                     "customer_phone_number": rental[3], "car_plate": rental[4]})
 
-            # Writes the dictionary to the file
-            json_object = json.dumps(rental_history, indent=4, ensure_ascii=False)
-            file.write(json_object)
+            try:
+                with open(file_path, "w+", encoding="UTF8") as file:
+                    # Writes the dictionary to the file
+                    json_object = json.dumps(rental_history, indent=4, ensure_ascii=False)
+                    file.write(json_object)
+                print("Successfully exported to file " + file_name)
+                return
+            except Exception:
+                print(f"ERROR {Exception}: Something went wrong while writing to the file!")
+                return
